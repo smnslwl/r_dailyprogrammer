6e43c4742d519b804b616d1996c29bd1da183acb
Samin Silwal <smnslwl@gmail.com>
2015-09-24 23:22:38 +0545

    Add graceful exit if filename not found, and small fixes

--- 231i/main.cpp
+++ 231i/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <vector>
 
-// Returns true if the three cards for a valid set
+// Returns true if the three cards form a valid set
 bool is_valid_set(const std::string& c1, const std::string& c2, 
                     const std::string& c3)
 {
@@ -15,7 +15,7 @@ bool is_valid_set(const std::string& c1, const std::string& c2,
 }
 
 int main(int argc, char* argv[])
-{    
+{
     if (argc < 2) {
         std::cout << "Usage: " << argv[0] << " FILENAME\n";
         return 0; 
@@ -24,12 +24,15 @@ int main(int argc, char* argv[])
     std::ifstream               f(argv[1]);
     std::string                 line;
     std::vector<std::string>    c;
+    const uint                  n = 12;
     
-    if (f.good()) while (std::getline(f, line)) c.push_back(line);
+    if (f.good()) 
+        while (std::getline(f, line)) c.push_back(line);
+    else return 0;
 
-    for (auto i = 0; i < 12; ++i)
-        for (auto j = i + 1; j < 12; ++j)
-            for (auto k = j + 1; k < 12; ++k)
+    for (uint i = 0; i < n; ++i)
+        for (uint j = i + 1; j < n; ++j)
+            for (uint k = j + 1; k < n; ++k)
                 if (is_valid_set(c[i], c[j], c[k])) 
                     std::cout << c[i] << " " << c[j] << " " << c[k] << "\n";
 }
