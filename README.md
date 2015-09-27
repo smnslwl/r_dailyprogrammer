e618fca5b6c877048865652dcb3100b1177e3e80
Samin Silwal <smnslwl@gmail.com>
2015-09-28 01:41:16 +0545

    Set up script to autogenerate README.md with list of challenges

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# /r/dailyprogrammer solutions
+
+# Solutions to /r/dailyprogrammer challenges
 
 https://www.reddit.com/r/dailyprogrammer/
+
+
+## Solutions
+
+ Challenge # | Date | Title | Solutions 
+-------------|------|-------|-----------
+231 Hard | 2015-09-11 | [Eight Husbands for Eight Sisters ](https://www.reddit.com/r/dailyprogrammer/comments/3kj1v9/20150911_challenge_231_hard_eight_husbands_for/) | [Python](231h/main.py)
+231 Intermediate | 2015-09-09 | [Set game solver](https://www.reddit.com/r/dailyprogrammer/comments/3ke4l6/20150909_challenge_231_intermediate_set_game/) | [C++](231i/main.cpp)
+232 Easy | 2015-09-14 | [Palindromes](https://www.reddit.com/r/dailyprogrammer/comments/3kx6oh/20150914_challenge_232_easy_palindromes/) | [C](232e/main.c)
+232 Hard | 2015-09-18 | [Redistricting Voting Blocks](https://www.reddit.com/r/dailyprogrammer/comments/3lf3i2/20150918_challenge_232_hard_redistricting_voting/) | [C++](232h/main.cpp)
+232 Intermediate | 2015-09-16 | [Where Should Grandma's House Go?](https://www.reddit.com/r/dailyprogrammer/comments/3l61vx/20150916_challenge_232_intermediate_where_should/) | [C++](232i/main.cpp)
+233 Easy | 2015-09-21 | [The house that ASCII built](https://www.reddit.com/r/dailyprogrammer/comments/3ltee2/20150921_challenge_233_easy_the_house_that_ascii/) | [Python](233e/main.py)
+233 Intermediate | 2015-09-23 | [Game of Text Life](https://www.reddit.com/r/dailyprogrammer/comments/3m2vvk/20150923_challenge_233_intermediate_game_of_text/) | [C++](233i/main.cpp)
+
+
+## Licence
+
+Pretty simple, see [LICENCE.txt](LICENCE.txt) for details.
+
---
+++ make_README.py
@@ -0,0 +1,79 @@
+import os
+import re
+
+README      =   """
+# Solutions to /r/dailyprogrammer challenges
+
+https://www.reddit.com/r/dailyprogrammer/
+
+
+## Solutions
+
+ Challenge # | Date | Title | Solutions 
+-------------|------|-------|-----------
+{solution_list}
+
+## Licence
+
+Pretty simple, see [LICENCE.txt](LICENCE.txt) for details.
+
+"""
+
+SOLUTION    =   '{challenge_id} | {date} | [{title}]({url}) | {solutions}\n'
+
+IS_DIR      =   re.compile(r'[0-9]+[eihw]')
+
+BASE_DIR    =   ''
+
+LEVELS      =   { 
+                    'e'     : 'Easy',
+                    'i'     : 'Intermediate',
+                    'h'     : 'Hard',
+                    'w'     : 'Weekly',
+                }
+    
+NAMES       =   [
+                    'main',
+                    'Main',
+                ]
+
+LANGS       =   {
+                    '.py'    : 'Python',
+                    '.c'     : 'C',
+                    '.cpp'   : 'C++',
+                    '.hs'    : 'Haskell',
+                }
+
+
+dirs = [x for x in os.listdir() if os.path.isdir(x) and IS_DIR.fullmatch(x)]
+
+solution_list = ''
+
+for challenge in sorted(dirs):
+    path = os.path.join(BASE_DIR, challenge)
+
+    with open(os.path.join(path, 'README.md')) as f:
+        lines = [x for x in f.read().splitlines() if x][:4]
+    title, date, url = lines[0][1:], lines[2], lines[3]
+    challenge_id = '{} {}'.format(challenge[:-1], LEVELS[challenge[-1]])
+    
+
+    files = [x for x in os.listdir(path) 
+                if os.path.isfile(os.path.join(path, x))]
+
+    solutions = []
+    for filename in files:
+        root, ext = os.path.splitext(filename)
+        if root in NAMES and ext in LANGS:
+            solutions.append('[{}]({})'.format(
+                                LANGS[ext], (os.path.join(path, filename))
+                            ))
+    
+    
+    solution_list += SOLUTION.format(
+                        challenge_id=challenge_id, date=date, url=url, 
+                        title=title, solutions=', '.join(solutions)
+                     )
+
+with open('README.md', 'w') as f:
+    f.write(README.format(solution_list=solution_list))
